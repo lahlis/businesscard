1ea15cacdd7a4a7deef5f604697a5de72425bebd
Laís Nascimento <lais.an@yahoo.com.br>
2022-07-19 17:57:16 -0300

    Project done

---
+++ app/src/main/java/me/dio/businesscard/App.kt
@@ -0,0 +1,10 @@
+package me.dio.businesscard
+
+import android.app.Application
+import me.dio.businesscard.data.AppDataBase
+import me.dio.businesscard.data.BusinessCardRepository
+
+class App :Application() {
+    val database by lazy { AppDataBase.getDataBase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
+}
